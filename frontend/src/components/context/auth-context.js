bc1cfd9731958273f760a8fff9d84d757e3f1b47
asus <someshanjana26@gmail.com>
2023-04-29 15:36:33 +0530

    adding vendor and fetching the dispatched data for mohalla users

--- frontend/src/components/context/auth-context.js
+++ frontend/src/components/context/auth-context.js
@@ -9,8 +9,10 @@ const AuthContext = React.createContext({
   logout: () => {},
   userEmail: "",
   userType: "",
+  userId: "",
+
   setUserType: (type) => {},
-  setUserRoleType: (type, email) => {},
+  setUserRoleType: (type, email, id) => {},
 });
 
 const calculateRemainingTime = (expirationTime) => {
@@ -26,9 +28,10 @@ export const AuthContextProvider = (props) => {
   const [token, setToken] = useState("");
   const [userType, setUserType] = useState("");
   const [userEmail, setUserEmail] = useState("");
+  const [userId, setUserId] = useState("");
 
   let initialToken;
-  
+
   useEffect(() => {
     // Perform localStorage action
     initialToken = localStorage.getItem("token");
@@ -37,15 +40,18 @@ export const AuthContextProvider = (props) => {
 
   let localType;
   let localEmail;
+  let localId;
 
   useEffect(() => {
     // Perform localStorage action
     localType = localStorage.getItem("type");
     localEmail = localStorage.getItem("email");
+    localId = localStorage.getItem("user_id");
 
     setUserType(localType);
     setUserEmail(localEmail);
-  }, [localType, localEmail]);
+    setUserId(localId);
+  }, [localType, localEmail, localId]);
 
   const userIsLoggedIn = !!token;
 
@@ -56,6 +62,8 @@ export const AuthContextProvider = (props) => {
 
     localStorage.removeItem("type");
     localStorage.removeItem("email");
+
+    localStorage.removeItem("user_id");
 
     // if the timer was saved thenwe are clearing the timer
     if (logoutHandler) {
@@ -78,17 +86,21 @@ export const AuthContextProvider = (props) => {
     logoutTimer = setTimeout(logoutHandler, remianingTime);
   };
 
-  const setUserRoleType = (type, email) => {
+  const setUserRoleType = (type, email, userId) => {
     console.log("inside setting data");
 
     localStorage.setItem("type", type);
     localStorage.setItem("email", email);
+    localStorage.setItem("user_id", userId);
 
     const localType = localStorage.getItem("type");
     const localEmail = localStorage.getItem("email");
 
+    const localId = localStorage.getItem("user_id");
+
     setUserType(localType);
     setUserEmail(localEmail);
+    setUserId(localId);
   };
 
   const contextValue = {
@@ -99,6 +111,7 @@ export const AuthContextProvider = (props) => {
     setUserRoleType: setUserRoleType,
     userType: userType,
     userEmail: userEmail,
+    userId: userId,
   };
 
   return (
--- frontend/src/procurement_inventory/vendors/NewVendor.js
+++ frontend/src/procurement_inventory/vendors/NewVendor.js
@@ -1,55 +1,173 @@
-import React from 'react'
-import { Card, Row, Col, Button, Input, TimePicker, Checkbox, Radio } from 'antd'
+import React, { useState, useContext } from "react";
+
+import {
+  Card,
+  Row,
+  Col,
+  Button,
+  Input,
+  TimePicker,
+  Checkbox,
+  Radio,
+  Modal,
+} from "antd";
 
 const NewVendor = () => {
+  const [vendorName, setVendorName] = useState();
+  const [vendorEmail, setVendorEmail] = useState();
+  const [vendorAddress, setVendorAddress] = useState();
+  const [closingTime, setClosingTime] = useState();
+  const [openingTime, setOpeningTime] = useState();
+  const [visible, setVisible] = useState(false);
+
+  const createNewVendor = async () => {
+    const selectedTimeOpening = openingTime ? new Date(openingTime) : null;
+    const selectedTimeClosing = closingTime ? new Date(closingTime) : null;
+
+    const finalOpeningTime = selectedTimeOpening
+      ? selectedTimeOpening.toLocaleTimeString()
+      : "";
+
+    const finalClosingTime = selectedTimeClosing
+      ? selectedTimeClosing.toLocaleTimeString()
+      : "";
+
+    if (
+      vendorAddress &&
+      vendorEmail &&
+      vendorName &&
+      finalClosingTime &&
+      finalOpeningTime
+    ) {
+      try {
+        console.log("inside");
+        const data = await fetch("http://localhost:5001/vendor", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            mkuser_email: "sujeet@gmail.com",
+            vendor_name: vendorName,
+            opening_time: finalOpeningTime,
+            closing_time: finalClosingTime,
+            email: vendorName,
+            address: vendorAddress,
+          }),
+        });
+
+        if (data) {
+          const res = await data.json();
+          setVisible(true);
+          setVendorEmail("");
+          setVendorName("");
+          setVendorAddress("");
+          setOpeningTime("");
+          setClosingTime("");
+
+          console.log(res);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div>
-        <Card style={{ padding:'1%', border:'1px solid grey' }} bordered={true}>
-            <Row>
-                <Col xs={12} xl={12} style={{ fontSize: '200%' }}><i class="fa-solid fa-arrow-left"></i>&nbsp;&nbsp;&nbsp;New Vendor</Col>
-                <Col xs={12} xl={12}>
-                    <center><Button type='primary' danger>Cancel</Button></center>
-                </Col>
-            </Row>
+      <Modal
+        visible={visible}
+        onOk={() => setVisible(false)}
+        onCancel={() => setVisible(false)}
+        footer={[
+          <Button key="ok" type="primary" onClick={() => setVisible(false)}>
+            OK
+          </Button>,
+        ]}
+      >
+        <div style={{ textAlign: "center" }}>
+          <h2 style={{ color: "#52c41a" }}>Success!</h2>
+          <p>Vendor Added Successfully</p>
+        </div>
+      </Modal>
+
+      <Card style={{ padding: "1%", border: "1px solid grey" }} bordered={true}>
+        <Row>
+          <Col xs={12} xl={12} style={{ fontSize: "200%" }}>
+            <i class="fa-solid fa-arrow-left"></i>&nbsp;&nbsp;&nbsp;New Vendor
+          </Col>
+          <Col xs={12} xl={12}>
+            <center>
+              <Button type="primary" danger>
+                Cancel
+              </Button>
+            </center>
+          </Col>
+        </Row>
+      </Card>
+      <div style={{ width: "80%", padding: "3%" }}>
+        <Card
+          style={{ border: "2px solid orange" }}
+          className="dongle-font-class"
+        >
+          <label style={{ fontSize: "150%" }}>
+            <u>Vendor Details</u>
+          </label>
+          <br />
+          <table style={{ width: "100%", fontSize: "130%" }} cellPadding={20}>
+            <tr>
+              <td>
+                Vendor Name:{" "}
+                <Input
+                  value={vendorName}
+                  onChange={(e) => setVendorName(e.target.value)}
+                  placeholder="Eg: VK General store, Brahma store, etc.."
+                ></Input>
+              </td>
+              <td>
+                Email:{" "}
+                <Input
+                  value={vendorEmail}
+                  onChange={(e) => setVendorEmail(e.target.value)}
+                  placeholder="Eg: brahma@gmail.com, vkstore@hotmail.com, etc.."
+                ></Input>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Address:{" "}
+                <Input
+                  value={vendorAddress}
+                  onChange={(e) => setVendorAddress(e.target.value)}
+                  placeholder="Must include Street name, locality, city and country.."
+                ></Input>
+              </td>
+              <td>
+                <Row>
+                  <Col xs={12} xl={12}>
+                    Opening time: <br />
+                    <TimePicker
+                      value={openingTime}
+                      onChange={(time) => setOpeningTime(time)}
+                    ></TimePicker>
+                  </Col>
+                  <Col xs={12} xl={12}>
+                    Closing time: <br />
+                    <TimePicker
+                      value={closingTime}
+                      onChange={(time) => setClosingTime(time)}
+                    ></TimePicker>
+                  </Col>
+                </Row>
+              </td>
+            </tr>
+          </table>
         </Card>
-        <div style={{ width: '80%', padding:'3%' }}>
-            <Card style={{ border: '2px solid orange' }} className='dongle-font-class'>
-                <label style={{ fontSize: '150%' }}>
-                    <u>Vendor Details</u>
-                </label>
-                <br/>
-                <table style={{ width: '100%', fontSize: '130%' }} cellPadding={20}>
-                    <tr>
-                        <td>
-                            Vendor Name: <Input placeholder='Eg: VK General store, Brahma store, etc..'></Input>
-                        </td>
-                        <td>
-                            Email: <Input placeholder='Eg: brahma@gmail.com, vkstore@hotmail.com, etc..'></Input>
-                            
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Address: <Input placeholder='Must include Street name, locality, city and country..'></Input>
-                        </td>
-                        <td>
-                            <Row>
-                                <Col xs={12} xl={12}>Opening time: <br/><TimePicker></TimePicker></Col>
-                                <Col xs={12} xl={12}>Closing time: <br/><TimePicker></TimePicker>
-                                </Col>
-                            </Row>
-                        </td>
-                    </tr>
-                    
-                </table>
-            </Card>
-            
 
+        <br />
+        <br />
 
-            <br/><br/>
-            
-            
-            <Card style={{ border: '2px solid orange' }} className='dongle-font-class'>
+        {/* <Card style={{ border: '2px solid orange' }} className='dongle-font-class'>
                 <label style={{ fontSize: '150%' }}>
                     <u>Availability</u>
                 </label>
@@ -76,14 +194,17 @@ const NewVendor = () => {
                     </Row>
                 </Radio.Group>
                         
-            </Card>
+            </Card> */}
 
-            <br/><br/>
+        <br />
+        <br />
 
-            <Button type='primary'> ADD VENDOR </Button>
-        </div>
+        <Button onClick={createNewVendor} type="primary">
+          ADD VENDOR
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewVendor
+export default NewVendor;
