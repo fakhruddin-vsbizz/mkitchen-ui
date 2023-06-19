4e78620f741e52f40c479fc83055197e08d14752
abdulkadervsbizz <abdulkadervsbizz@gmail.com>
2023-06-19 11:33:36 +0530

    average cost of items

--- routes/reportRouter.js
+++ routes/reportRouter.js
@@ -7,3 +7,4 @@ module.exports = router
     .get('/report/total-inventory', report.getTotalItems)
     .get('/report/total-purchases', report.getPurchaseReport)
     .get('/report/vendor-purchases', report.getPurchsedByVendorReport)
+    .get('/report/avg-item-cost/:id', report.getAvgCost)
